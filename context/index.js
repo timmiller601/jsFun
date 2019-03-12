efaf6fda4f675e887939973f791b1b7670e93358
tim miller <tmiller12@gmail.com>
2019-03-11 20:10:21 -0600

    Finished context and updated annotations

--- context/index.js
+++ context/index.js
@@ -16,11 +16,17 @@ const context = {
 
 
     // What is the value of `this` when we call ship.fly()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // 'this' references the global window object. The const variable 
+    // fly is an ES6 arrow function and the value of 'this' is set when
+    // the fuction is created, which then points to the global window object. 
+    // If fly() was declared on line 11 inside the constructor function then
+    // 'this' would reference ship, becuase fyl() would be declared when 
+    // the constructor function is invoked and therefore the current
+    // context of this would be the newly instanciated object ship.
   },
 
   exerciseB() {
@@ -30,11 +36,14 @@ const context = {
     }
     
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation:
-    // Write your annotation here as a comment
+    // 'this' will reference the global window object because fn() is written
+    // as an ES5 function, which sets the value of 'this' when invoked. In
+    // this case when we call fn() it is not called as a method on a
+    // particiular object, therefore 'this' refers to the global object.
   },
 
   exerciseC() {
@@ -49,11 +58,14 @@ const context = {
     el.addEventListener('click', car.getInfo);
 
     // What is the value of `this` when a user clicks on our element and car.getInfo() is triggered?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'el';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' will reference the const variable el, which is the value of the
+    // element selected from the DOM because of rule #2 when a function
+    // is invoked as a method with dot notation 'this' references the obj
+    // to the left of the dot.
   },
 
   exerciseD() {
@@ -70,11 +82,15 @@ const context = {
 
 
     // What is the value of `this` when we call dog.getBreed()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' will reference the global window object. When dog.getBreed()
+    // is executed, innerFuntion() is invoked which is an ES5 function and
+    // therefore sets the value of 'this' upon execution, not creation
+    // and since innerFunction() is not executed as a method of the dog
+    // object 'this' references the global window object. 
   },
 
   exerciseE() {
@@ -86,11 +102,14 @@ const context = {
 
 
     // What is the value of `this` when we call fn()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' references the global window object. fn() is writen as an ES6
+    // function, which sets the value of 'this' upon creation. fn() is not executed as a method on
+    // an object and is not invoked in function code with the keyword new, therefore the default
+    // 'this' refers to the global window object.
   },
 
   exerciseF() {
@@ -109,11 +128,13 @@ const context = {
     const storm = new Hero('Ororo', 'weather control', true);
 
     // What is the value of `this` when we call storm.identifyHero()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'instance of Hero';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' references the newly created object instance becuase of
+    // rule #3, which says when using the new operator 'this' refers to the
+    // newly created object
   },
 
   exerciseG() {
@@ -137,11 +158,13 @@ const context = {
 
 
     // What is the value of `this` when we call monopoly.restart()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'global window object';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' references the global window object. When restart() is invoked as a method of 
+    // monopoly, setTimeout() is imediately invoked. Since 'this' is set in ES5 when the function
+    // is invoked, 'this' refers to the global object in the setTimeout function
   },
 
   exerciseH() {
@@ -157,11 +180,17 @@ const context = {
     obj.method();
 
     // What is the value of `this` when we call obj.arrowFunction()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'obj';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment
+    // 'this' references obj. On line 180 method() is invoked as a
+    // method of obj, in the current context 'this' references obj 
+    // and we move up to line 176. In the functional scope we 
+    // reassign this.arrowFunction (which was previously a property 
+    // with a value of null), to an ES6 arrow function. Since the 
+    // value of 'this' is determined when an ES6 arrow function 
+    // is created, 'this' refers to obj
   },
 
   exerciseI() {  
@@ -180,11 +209,13 @@ const context = {
     }, poets);
 
     // What is the value of `this` that gets returned on each iteration of poets.map()?
-    const result = 'REPLACE WITH YOUR RESULT HERE';
+    const result = 'poets';
     return result;
 
     // Annotation: 
-    // Write your annotation here as a comment. Annotation should include explanation regarding the second argument of `poets` that is being passed
+    // 'this' references poets becuase the method map() is invoked on the poets 
+    // object and when executing a functon as a method of an object, the 
+    // value of 'this' refers to that object.
   },
 
   exerciseJ() {
